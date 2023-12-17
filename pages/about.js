be1c0421a7d79b77bbd0658109f17af26acc35e4
sara moosavi <mohammadmousavi@192.168.1.104>
2023-12-17 20:53:58 +0300

    oops

--- pages/about.js
+++ pages/about.js
@@ -7,11 +7,7 @@ const about = () => {
   return (
     <div className='text-gray-400 text-xl font-semibold flex flex-1 flex-col  p-8 justify-start'>
         <Link href="/" >AGHAJOON MARKET</Link>
-     <div className='w-full flex flex-col items-center my-20 h-full '>
-        {orders.map(i=>(
-          <div>{i.customer.email}</div>
-        ))}
-     </div>
+    
     </div>
     
   )
